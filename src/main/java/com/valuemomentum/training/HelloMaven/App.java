b816e5db160032daa424f5ed27e51f18fa8bf038
Harshita <harshita.ambekar@gmail.com>
2021-02-18 15:18:31 +0530

    Commiting Maven code

---
+++ src/main/java/com/valuemomentum/training/HelloMaven/App.java
@@ -0,0 +1,18 @@
+package com.valuemomentum.training.HelloMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "****Banking Application****" );
+        Customer c1= new Customer(101,"BTS",5000);
+        c1.display();
+    
+    
+    }
+    
+}
---
+++ src/main/java/com/valuemomentum/training/HelloMaven/Customer.java
@@ -0,0 +1,24 @@
+package com.valuemomentum.training.HelloMaven;
+
+public class Customer {
+	
+	int accNo;
+	String name;
+	double balance;
+	
+	public Customer(int accNo,String name,double balance)
+	{
+		super();
+		this.accNo= accNo;
+		this.name= name;
+		this.balance= balance;
+	}
+	void display()
+	{
+	    System.out.println("*******Customer Details**********");
+	    System.out.println(this.accNo+" "+this.name+" "+this.balance);
+	}
+	
+
+}
+
